8497afd8b49dadfcceabe7f2c0b43270110e4abd
snehal kumbhar <99393067+snehalkumbhar1101@users.noreply.github.com>
2022-04-27 16:46:48 +0530

    Add files via upload

---
+++ tests/touch globalSetup.js
@@ -0,0 +1,9 @@
+//globalSetup.js
+require('@babel/register');
+
+const server = require('../../src/app').default;
+
+module.exports = async () => {
+    global.httpServer = server;
+    await global.httpServer.listen();
+};
